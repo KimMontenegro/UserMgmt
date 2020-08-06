36231c4c72809fad70fdcca09c5180964ef7dec9
Kimberly Mae Montenegro <mkimberlymae0812@gmail.com>
2020-08-06 11:21:18 +0800

    changes in url

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -13,75 +13,66 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   //createState is the method we override
-  MyAppState createState() => MyAppState();
+  MyAppState createState() => new MyAppState();
 }
 
 //Pass the state in here
 class MyAppState extends State<MyApp> {
-  TextEditingController nameContoller = TextEditingController();
+  final String url = "http://192.168.0.18:3001/users";
   List data;
-
-  createAlertDialog(BuildContext context){
-    return showDialog(context: context, builder: (context) {
-      return AlertDialog(
-        title: Text('done'),
-        content: TextField(
-          controller: nameContoller,
-        ),
-        actions: <Widget>[
-          MaterialButton(
-            elevation: 5.0,
-            child: Text('OK'),
-            onPressed: () {
-
-              Navigator.of(context).pop(nameContoller.text.toString());
-            },
-          )
-        ],
-      );
-    });
-  }
-
 
   @override
   void initState() {
     //initial state of the widget
     super.initState();
-    this.getData();
+    this.getJsonData();
   }
 
   //Future represent a potential value/error that will be available at some time in the future
-  // ignore: missing_return
-  Future<String> getData() async {
+  Future<String> getJsonData() async {
     //await - wait until we get the data
-    var response = await http.get(
-        Uri.encodeFull("192.168.0.18:3001/users"), //to encode the url
+    final response = await http.get(Uri.encodeFull(url), //to encode the url
         headers: {"Accept": "application/json"}); //only accept json response
-
-    print(response.body); //will not work unless there's a response.
-
+    // if (response.statusCode == 200) {
+    //   var jsonResponse = jsonDecode(response.body);
+    //   data = jsonResponse['results'];
+    //   print(response.body);
+    // } else {
+    //   print('Request failed with status: ${response.statusCode}.');
+    // }
     setState(() {
       //rebuild the widget and modify the app state
       var convert = jsonDecode(response.body); //convert the data to json
       data = convert['results']; //to get the converted json data in mockoon
     });
+
+    return (response.body);
+    //print(response.body); //will not work unless there's a response.
   }
 
   @override
   Widget build(BuildContext context) {
     //Scaffold provides drawers, snack bars, bottom sheets
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('List of Users'),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('List of Users'),
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(20),
+      body: new ListView.builder(
         //if data is null then 0 otherwise count the length of data
         itemCount: data == null ? 0 : data.length,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            child: Center(
-              child: Text(data[index]['name']),
+          return new Container(
+            child: new Center(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new Card(
+                    child: new Container(
+                      child: new Text(data[index]['name']),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
